c73638ad22065ebb04d2781c7cfc445456081eab
yoojat <yooja.ty@gmail.com>
2019-08-05 23:44:27 +0900

    회사소개 메뉴 스타일

--- pages/companyInfo/companyInfoPresenter.js
+++ pages/companyInfo/companyInfoPresenter.js
@@ -16,13 +16,13 @@ const TopBanner = styled.div`
 const BannerTitle = styled.div`
   color: white;
   font-size: 2rem;
-  margin-left: 3rem;
+  margin-left: 10vw;
   margin-bottom: 0.7rem;
 `;
 const BannerSubTitle = styled.div`
   color: white;
   font-size: 1.1rem;
-  margin-left: 3rem;
+  margin-left: 10vw;
 `;
 const TopBannerModal = styled.div`
   height: 110px;
@@ -31,6 +31,83 @@ const TopBannerModal = styled.div`
   top: 100px;
   background-color: rgba(0, 0, 0, 0.5); /*살짝 투명한 검정으로 배경색*/
   padding-top: 60px;
+`;
+
+const ContentSection = styled.div`
+  text-align: left;
+  margin-top: 3rem;
+  @media (max-width: 600px) {
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+  }
+`;
+
+const LeftMenu = styled.div`
+  display: inline-block;
+  width: 20%;
+  margin-left: 2.5%;
+  margin-right: 2.5%;
+  @media (max-width: 600px) {
+    display: block;
+    width: 100%;
+    margin-left: inherit;
+    margin-right: inherit;
+  }
+`;
+const RightContentContainer = styled.div`
+  display: inline-block;
+  width: 70%;
+  margin-left: 2.5%;
+  margin-right: 2.5%;
+  background-color: powderblue;
+  @media (max-width: 600px) {
+    width: 95%;
+  }
+`;
+
+const MenuContainer = styled.div``;
+const MenuMainTitle = styled.div`
+  color: white;
+  background-color: ${props => props.theme.deepBlue};
+  line-height: 3rem;
+  padding-left: 2rem;
+  font-size: 1.3rem;
+  margin-bottom: 1.1rem;
+  @media (max-width: 600px) {
+    display: none;
+  }
+`;
+const MenuSubContainer = styled.div`
+  @media (max-width: 600px) {
+    text-align: center;
+  }
+`;
+
+const MenuItem = styled.div`
+  &:hover {
+    background-color: ${props => props.theme.lightBlue};
+    color: white;
+    cursor: pointer;
+  }
+  line-height: 3rem;
+  padding-left: 2rem;
+  font-size: 1.1rem;
+  background-color: ${props => props.theme.whiteGray};
+  border-bottom: 0.1rem solid #dedede;
+  @media (max-width: 600px) {
+    display: inline-block;
+    padding-left: inherit;
+    margin-left: 1.5rem;
+    margin-right: 2rem;
+    background-color: inherit;
+    border-bottom: inherit;
+    font-size: 0.8rem;
+
+    &:hover {
+      background-color: inherit;
+      color: inherit;
+    }
+  }
 `;
 
 export default ({}) => (
@@ -47,5 +124,19 @@ export default ({}) => (
         </BannerSubTitle>
       </TopBannerModal>
     </TopBannerContainer>
+    <ContentSection>
+      <LeftMenu>
+        <MenuContainer>
+          <MenuMainTitle>회사소개</MenuMainTitle>
+
+          <MenuSubContainer>
+            <MenuItem>인사말</MenuItem>
+            <MenuItem>주요사업</MenuItem>
+            <MenuItem>오시는 길</MenuItem>
+          </MenuSubContainer>
+        </MenuContainer>
+      </LeftMenu>
+      <RightContentContainer>컨텐츠 섹션</RightContentContainer>
+    </ContentSection>
   </>
 );
--- theme.js
+++ theme.js
@@ -1,7 +1,10 @@
 const theme = {
   lightGray: "#8f8f8f",
   black: "#1b1c20",
-  skyBlue: "#3498db"
+  skyBlue: "#3498db",
+  deepBlue: "#003777",
+  lightBlue: "#3395d2",
+  whiteGray: "#f9f9f9"
 };
 
 export default theme;
